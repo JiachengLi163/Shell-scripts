32de602da70391b1888c9fbee2de305e23f2e396
Jiacheng Li <jiachengli@ieiworld.com>
2021-06-24 17:37:39 +0800

    add a disk read&write test scripts

---
+++ disk_rw.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+for ((i=1; i <= 100; i++))
+do
+	echo "prepare read test"
+	sh -c "sync && echo 3 > /proc/sys/vm/drop_caches" #clean Buffer
+	sudo dd if=/dev/sda of=/dev/null bs=1M count=300
+	echo "read successfully"
+
+	echo "prepare write test"
+	sh -c "sync && echo 3 > /proc/sys/vm/drop_caches" #clean Buffer
+	sudo dd if=/dev/zero of=/home/ljc/test.txt bs=1M count=100 #/home/ljc: the disk is hung in the path
+	echo "write successfully"
+	rm /home/ljc/test.txt
+	echo $i 
+	echo 
+done
+
